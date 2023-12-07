a9f4123cd44db49ed2db0303b2e91456cb2af370
Alberto Novo <noveiras@gmail.com>
2023-12-07 11:01:56 -0500

    Create project template

---
+++ MyMood/MyMood/MyMoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoodApp.swift
+//  MyMood
+//
+//  Created by Alberto Novo Garrido on 2023-12-07.
+//
+
+import SwiftUI
+
+@main
+struct MyMoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
